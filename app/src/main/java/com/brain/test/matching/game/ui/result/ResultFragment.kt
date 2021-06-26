74c1e12c5436cb21ca7db3b467fa5f82bea37995
GUPTA SHUBHAM <shubham.gupta@unifa-e.com>
2021-06-26 22:46:44 +0900

    show game summary

--- app/src/main/java/com/brain/test/matching/game/ui/result/ResultFragment.kt
+++ app/src/main/java/com/brain/test/matching/game/ui/result/ResultFragment.kt
@@ -1,23 +1,23 @@
 package com.brain.test.matching.game.ui.result
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.brain.test.matching.game.R
+import com.brain.test.matching.game.databinding.FragmentResultBinding
 
 /***
  * A fragment to show the game result and provide an option to restart the game
  */
-class ResultFragment : Fragment() {
+class ResultFragment : Fragment(R.layout.fragment_result) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+    private val navArgs: ResultFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentResultBinding.bind(view)
+        binding.tvSummary.text =
+            getString(R.string.message_results, navArgs.score, navArgs.timeLeft)
     }
-
 }
